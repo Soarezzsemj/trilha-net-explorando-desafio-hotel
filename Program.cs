ab8bb65209caa73384da7182c28d7b04fe01b78a
Soarezzsemj <popcornor6topster@outlook.com>
2025-10-18 15:11:14 -0300

    finalizado?

--- Program.cs
+++ Program.cs
@@ -8,18 +8,24 @@ List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
 Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+
+
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+// hospedes.Add(p3);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 5, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 11);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Capacidade da suite: {reserva.Suite.Capacidade}");
+Console.WriteLine($"Valor da diaria: {reserva.Suite.ValorDiaria}");
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
